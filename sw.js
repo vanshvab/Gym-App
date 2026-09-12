948a10d9d5da6d738c79ce6bcf422609753a0a17
vanshvab <96187803+vanshvab@users.noreply.github.com>
2026-09-12 20:26:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const C='cutmeso-v1';
+const F=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(F).catch(()=>{})));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const cp=res.clone();
+      caches.open(C).then(c=>c.put(e.request,cp)).catch(()=>{});
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
